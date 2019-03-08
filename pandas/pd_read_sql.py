d364e522d57e8997db158ddcc0c5421715db3126
Li huimin <362169885@qq.com>
2019-03-08 10:05:27 +0800

    update read_sql

--- pandas/pd_read_sql.py
+++ pandas/pd_read_sql.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import pymssql
+import matplotlib.pyplot as plt
 
-sql='select * from mm_item'
-con=pymssql.connect(host='192.168.0.126',user='sa',password='2018@Ikahe',database='X1_CORE_PROD',charset="utf8")
+from  pyecharts import  Map
+
+sql='SELECT [local],COUNT([local]) count FROM mm_customer_service GROUP BY [local]'
+con=pymssql.connect(host='*****',user='sa',password='*******',database='***',charset="utf8")
 
 data_sql=pd.read_sql(sql,con)
+x=data_sql['local'].values.tolist()
+y=data_sql['count'].values.tolist()
+#print(x,y)
+map = Map("分布省份", width=1200, height=600,background_color='#404a59')
+map.add(
+   '',
+   x,
+   y,
+   maptype='china',
+   is_visualmap=True,
+   symbol_size=10,
+   visual_text_color='#fff'
+)
 
-print(type(data_sql),data_sql)
+map.render('全国个点.html')
